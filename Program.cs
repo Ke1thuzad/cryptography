11f8c150d306977df13fa5fb7480b1626b42c4a6
Dmitriy <dolbus1235@mail.ru>
2025-10-11 00:36:44 +0300

    lab1task1 done

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Cryptography
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            byte[] data = [0b11001010, 0b10101101, 0b00110100, 0b11110001, 0b01011011, 0b10001100, 0b01100111, 0b00011101];
+            byte[] pBlock = [
+                58, 50, 42, 34, 26, 18, 10, 2,
+                60, 52, 44, 36, 28, 20, 12, 4,
+                62, 54, 46, 38, 30, 22, 14, 6,
+                64, 56, 48, 40, 32, 24, 16, 8,
+                57, 49, 41, 33, 25, 17, 9, 1,
+                59, 51, 43, 35, 27, 19, 11, 3,
+                61, 53, 45, 37, 29, 21, 13, 5,
+                63, 55, 47, 39, 31, 23, 15, 7
+            ];
+
+            byte[] permutedData = Utility.PermuteBits(data, pBlock, true, true);
+        
+            Console.Write("Hex: ");
+            foreach (byte b in permutedData)
+            {
+                Console.Write($"{b:X2} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Cryptography;
+
+public static class Utility
+{
+    public static byte[] PermuteBits(byte[] bytes, byte[] P, bool MSB = true, bool indexFromOne = true)
+    {
+        ArgumentNullException.ThrowIfNull(bytes);
+        ArgumentNullException.ThrowIfNull(P);
+
+        int totalBits = bytes.Length * 8;
+        int resultBits = P.Length;
+        byte[] result = new byte[(resultBits + 7) / 8];
+
+        for (int i = 0; i < resultBits; i++)
+        {
+            int sourcePos = P[i];
+            
+            if (indexFromOne) 
+                sourcePos--;
+
+            if (sourcePos < 0 || sourcePos >= totalBits)
+                throw new ArgumentOutOfRangeException();
+
+            int sourceByteIndex = sourcePos / 8;
+            int sourceBitIndex = sourcePos % 8;
+            
+            if (MSB) 
+                sourceBitIndex = 7 - sourceBitIndex;
+            
+            bool bit = (bytes[sourceByteIndex] & (1 << sourceBitIndex)) != 0;
+
+            int resultByteIndex = i / 8;
+            int resultBitIndex = i % 8;
+            
+            if (MSB) 
+                resultBitIndex = 7 - resultBitIndex;
+
+            if (bit)
+                result[resultByteIndex] |= (byte)(1 << resultBitIndex);
+            else
+                result[resultByteIndex] &= (byte)~(1 << resultBitIndex);
+        }
+
+        return result;
+    }
+}
